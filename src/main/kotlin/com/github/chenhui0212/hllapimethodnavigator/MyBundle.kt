a2baf2df9ba6e76f8c8fc798b97f553cae374bd7
GitHub Action <action@github.com>
2020-09-29 07:44:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/chenhui0212/hllapimethodnavigator/MyBundle.kt
+++ src/main/kotlin/com/github/chenhui0212/hllapimethodnavigator/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.chenhui0212.hllapimethodnavigator
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/chenhui0212/hllapimethodnavigator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/chenhui0212/hllapimethodnavigator/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.chenhui0212.hllapimethodnavigator.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chenhui0212.hllapimethodnavigator.MyBundle
 
 class MyProjectService(project: Project) {
 
